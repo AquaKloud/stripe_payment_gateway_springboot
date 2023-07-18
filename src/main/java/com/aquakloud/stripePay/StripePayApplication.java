c3151e47649f0058b3a84829284f18de51ff8cd3
Sanjeewa Yapa <aquakloud0829@gmail.com>
2023-07-18 15:02:27 +0530

    Add dependencies

---
+++ src/main/java/com/aquakloud/stripePay/StripePayApplication.java
@@ -0,0 +1,13 @@
+package com.aquakloud.stripePay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StripePayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StripePayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aquakloud/stripePay/StripePayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aquakloud.stripePay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StripePayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
